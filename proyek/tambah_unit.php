e1db9e6a9610de5f2aa04acc25e65d8aa5870365
alip <haikalsialip999@gmail.com>
2024-01-16 19:45:57 +0700

    update codingan 16-01-2024

---
+++ proyek/tambah_unit.php
@@ -0,0 +1,52 @@
+<?php 
+include "warifheader.php"
+?>
+
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+<div class="container mt-5 mb-5">
+
+
+<div class="row row-cols-1 row-cols-md-2 g-4">
+  <div class="col">
+    <div class="card" >
+      <img src="https://www.hino.co.id//assets/uploads/products/FM280JD_L_Final-022.png"  style="height: 24rem;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title ">Hino Ranger 500</h5>
+        <p class="card-text">Truk Yang Suport Untuk Kebutuhan Cargo Antar Kota Di Lenkapi Dengan Mesin Turbo Yang Tangguh</p>
+        <p class="card-text text-info text-md-start fw-semibold ">Spesifikasi </p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.hino.co.id//assets/uploads/products/FM340TH_HIRES.png"  style="height: 24rem;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">FM 350 TH Euro 4</h5>
+        <p class="card-text">Di Lengkapi Dengan Supensi dan Stabil Saat Membawa Beban Dalam Skala Besar</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.hino.co.id//assets/uploads/products/FM340PD_(1).png" style="height: 21rem;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Hino Jumbo 500 Pro</h5>
+        <p class="card-text">Akselerasi Yang Baik Saat Menanjak Dan Handle Rem Yang Baik Saat Menurun</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="https://www.hino.co.id//assets/uploads/products/d3df6e124ff9491ddbe05b09562dc56c.png" style="height: 21rem;" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">SG 280 TH Euro4</h5>
+        <p class="card-text">Ketangguhan Mesin Untuk Menopang Kebutuhan Cargo Yang Membutuhkan Kecepatan Tinggi</p>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
+
+</main>
